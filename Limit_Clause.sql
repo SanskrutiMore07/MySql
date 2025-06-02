45a309ba1fab1de872ad7146e709782669de439f
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-06-02 21:46:50 +0530

    sql

---
+++ Limit_Clause.sql
@@ -0,0 +1,13 @@
+USE NPPS;
+
+INSERT INTO STUDENT
+(rollno,name,marks,grade,city)
+VALUES
+(102,'Sarikha Patil',80,'B','Pune'),
+(103,'Sahil Mane',90,'A','Pune'),
+(104,'Atharv More',95,'A','Satara'),
+(105,'Kunal Mohite',70,'C','Pune'),
+(106,'Arya More',85,'A','Satara');
+
+select * from STUDENT
+where marks > 90;
