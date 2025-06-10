2d11e2db9583396ea8a60ceb479f69da95bbf997
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-06-10 23:04:23 +0530

    sql

---
+++ FOREIGN KEY.sql
@@ -0,0 +1,40 @@
+use NPPS;
+
+CREATE TABLE StudentInformation
+(
+ROLLNO INT PRIMARY KEY,
+NAME VARCHAR(50),
+MARKS INT NOT NULL,
+GRADE CHAR,
+AGE INT NOT NULL,
+CITY VARCHAR(20),
+);
+
+CREATE TABLE Department
+(
+ID INT PRIMARY KEY,
+NAME VARCHAR(50),
+);
+
+CREATE TABLE Teacher
+(
+ID INT,
+NAME VARCHAR(50),
+DPT_ID INT,
+FOREIGN KEY(DPT_ID) REFERENCES Department(ID),
+);
+
+INSERT INTO StudentInformation (ROLLNO,NAME,MARKS,GRADE,AGE,CITY)
+VALUES
+(101,'Rahul More',80,'B',18,'Karad'),
+(102,'Rohit More',85,'B',18,'Pune'),
+(103,'Rohit More',85,'B',18,'Karad'),
+(104,'Raj Mane',90,'A',19,'Satara'),
+(105,'Tanmay Sutar',90,'A',18,'Karad'),
+(106,'Suyash Jaktap',85,'B',18,'Satara'),
+(107,'Aryan Khot',70,'C',18,'Pune'),
+(108,'Omakar Mohite',87,'B',19,'Pune'),
+(109,'jay patil',97,'A',18,'Karad'),
+(110,'Ram More',95,'A',18,'Satara');
+select * from StudentInformation;
+
